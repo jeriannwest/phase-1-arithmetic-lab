251caf243efb5fbe74249dad6480e20bb4455bcd
+ <jeriannwest@yahoo.com>
2021-11-02 05:27:05 -0400

    commit tree f5e80201bf754ba65308f9ce1ebd42dc6dfb0ad3

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+const num1 = 62
+const num2 = 1
+const multiply = (num1  * num2);
+const random = Math.floor(Math.random(0)* 10);
+const num3 = 100
+const num4 = 6
+const mod = num3 % num4;
+const max = Math.max(1,15,20)
 // Write your code here
